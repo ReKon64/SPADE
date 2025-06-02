310a4aac05ee0dbe2af1dc4e4a8032ec805d167a
ReKon64 <mariamalek64@gmail.com>
2025-06-02 21:17:32 +0200

    refactor: simplify logging format by removing prefix from log messages

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def main():
     # Configure logging
     if args.memory:
         from core.logging import MemoryUsageFormatter
-        format = '%(asctime)s - %(levelname)s - [MEM: %(memory_usage)s] %(prefix)s - %(message)s'
+        format = '%(asctime)s - %(levelname)s - [MEM: %(memory_usage)s] - %(message)s'
         if args.realtime and args.verbose:
             log_level = min(logging.DEBUG, 15)  # 10
         elif args.realtime:
@@ -128,7 +128,7 @@ def main():
         root_logger.setLevel(log_level)
         root_logger.addHandler(handler)
     else:
-        format = '%(asctime)s - %(levelname)s - %(prefix)s - %(message)s'
+        format = '%(asctime)s - %(levelname)s - %(message)s'
         if args.realtime and args.verbose:
             log_level = min(logging.DEBUG, 15)  # 10
         elif args.realtime:
